76e1ba117baab56ed296f9c50fc8a311f89c38b4
bkrm3 <bkrm3598@gmail.com>
2024-10-04 19:04:30 +0500

    .

--- app/src/main/java/com/starzplay/entertainment/ui/fragments/detail/DetailFragment.kt
+++ app/src/main/java/com/starzplay/entertainment/ui/fragments/detail/DetailFragment.kt
@@ -1,31 +1,39 @@
 package com.starzplay.entertainment.ui.fragments.detail
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.starzplay.entertainment.R
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.starzplay.entertainment.databinding.FragmentDetailBinding
+import com.starzplay.entertainment.extension.getImageUrl
+import com.starzplay.entertainment.ui.base.BaseFragment
+import com.starzplay.starzlibrary.data.remote.ResponseModel.MoviesData
 
-class DetailFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = DetailFragment()
-    }
-
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
     private val viewModel: DetailViewModel by viewModels()
-
+    private lateinit var selectedMovie: MoviesData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+        arguments?.let {
+            selectedMovie = DetailFragmentArgs.fromBundle(it).selectedMovie
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+
+    }
+
+    private fun setupViews() = with(binding) {
+        title.text = selectedMovie.title
+        description.text = selectedMovie.overview
+        val imageUrl = requireContext().getImageUrl(selectedMovie.posterPath!!)
+        Glide.with(requireContext()).load(imageUrl) // URL of the image you want to load
+            .into(detailImage)
     }
 }
