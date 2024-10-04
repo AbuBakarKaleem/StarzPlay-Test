d8d77ff328368991dc4d3ca75ccc7628fd1af6a4
bkrm3 <bkrm3598@gmail.com>
2024-10-04 19:02:35 +0500

    Item click listener

--- app/src/main/java/com/starzplay/entertainment/extension/AppUtils.kt
+++ app/src/main/java/com/starzplay/entertainment/extension/AppUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.starzplay.starzlibrary.helper.Constants.BASE_URL
 
 fun Fragment.longToast(message: String) {
     Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
@@ -11,4 +12,9 @@ fun Fragment.longToast(message: String) {
 
 fun Context.getColorResource(colorId: Int): Int {
     return ContextCompat.getColor(this, colorId)
+}
+
+fun Context.getImageUrl(imagePath: String, isRemoveSlash: Boolean = true): String {
+    val resultString = if (isRemoveSlash) imagePath.removePrefix("/") else imagePath
+    return BASE_URL + resultString
 }
